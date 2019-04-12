0ae3fe1a3c0de5faa18cfc4e2d59db3b0d115d27
AntokhivYura <antokhivyura@gmail.com>
2019-04-12 15:49:01 +0300

    Change property for Chrome driver

--- src/test/java/com/softserve/task1/account/lock_unlock_user/LockUnlockUser.java
+++ src/test/java/com/softserve/task1/account/lock_unlock_user/LockUnlockUser.java
@@ -29,8 +29,8 @@ public class LockUnlockUser {
     @BeforeClass
     public void openBrowser() {
         //Set Properties
-        System.setProperty("webdriver.chrome.driver",
-                this.getClass().getResource("/chromedriver-windows-32bit.exe").getPath());
+        System.setProperty("webdriver.chrome.driver", "./lib/drivers/chromedriver.exe");
+        System.getProperty("webdriver.chrome.driver");
         //Create new WebDriver object
         driver = new ChromeDriver();
         //Set window --> maximize
