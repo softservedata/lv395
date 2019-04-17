be489c24529f0729dedc3bd36a3cdc5e527b9a6a
Yurii Kril <yuriykril7773@gmail.com>
2019-04-18 00:42:08 +0300

    Add deleteItemTest and remove comments

--- src/test/java/com/softserve/edu/LoginAndAddProductsTest.java
+++ src/test/java/com/softserve/edu/LoginAndAddProductsTest.java
@@ -17,14 +17,7 @@ import java.util.regex.Pattern;
 public class LoginAndAddProductsTest {
 
     protected ChromeDriver driver;
- /*   private int quanityValue;
-    private double total;
-    private double unitPrice;*/
-    private final String OPEN_CART_URL = "http://192.168.234.129/opencart/upload/";
-   /* protected String editField = "#content > form > div > table > tbody > tr:nth-child(1) > td:nth-child(4) > div > input";*/
-
-    public String DELETE = "#content > h2";// "#content > form > div > table > tbody > tr:nth-child(1) > td:nth-child(4) > div > span > button.btn.btn-danger";
-
+    protected final String OPEN_CART_URL = "http://192.168.234.129/opencart/upload/";
 
 
     @BeforeClass
@@ -39,6 +32,12 @@ public class LoginAndAddProductsTest {
 
     @AfterClass
     public void afterClass() throws Exception {
+        logIn();
+        driver.findElement(By.cssSelector("i[class='fa fa-shopping-cart']")).click();
+        while (driver.findElements(By.cssSelector("#content > form")).size() > 0){
+            driver.findElement(By.cssSelector("#content > form > div > table > tbody > tr:nth-child(1) > td:nth-child(4) > div > span > button.btn.btn-danger")).click();
+            driver.navigate().refresh();
+        }
         driver.quit();
     }
 
@@ -65,11 +64,6 @@ public class LoginAndAddProductsTest {
     // * * * * * * * * * * * * * * * * * * * * * * * * * L O G O U T * * * * * * * * * * * * * * * * * * * * * * * * * *
     @Test(priority = 5)
     public void logOut() {
-        driver.findElement(By.cssSelector("i[class='fa fa-shopping-cart']")).click();
-        while (driver.findElements(By.cssSelector("#content > form")).size() > 0){
-            driver.findElement(By.cssSelector("#content > form > div > table > tbody > tr:nth-child(1) > td:nth-child(4) > div > span > button.btn.btn-danger")).click();
-            driver.navigate().refresh();
-        }
         driver.findElement(By.cssSelector("#top-links > ul > li.dropdown > a")).click();
         driver.findElement(By.xpath("//a[contains(text(),'Logout')]")).click();
         driver.findElement(By.cssSelector("i[class='fa fa-shopping-cart']")).click();
@@ -98,73 +92,5 @@ public class LoginAndAddProductsTest {
         Assert.assertTrue(expectedRowCount > 2);
     }
 
-/*
-    // * * * * * * * * * * * * * * * * * * * * * R E F R E S H * B U T T O N * * * * * * * * * * * * * * * * * * * * * *
-    @Test (priority = 3)
-    public void checkRefreshButtonOnShoppingCartTest() throws Exception {
-        driver.findElement(By.cssSelector("i[class='fa fa-shopping-cart']")).click();
-        quanityValue = Integer.parseInt(driver.findElement(By.cssSelector(editField)).getAttribute("value"));
-        total = getFloatNumber(driver.findElement(By.cssSelector("#content > form > div > table > tbody > tr:nth-of-type(1) > td:nth-of-type(6)")).getText());
-        unitPrice = getFloatNumber(driver.findElement(By.cssSelector("#content > form > div > table > tbody > tr:nth-child(1) > td:nth-child(5)")).getText());
-        double expectedTotal = (double) Math.round((total + unitPrice + unitPrice) * 100) / 100;
-        driver.findElement(By.cssSelector(editField)).clear();
-        String newQuanityValue = String.valueOf(quanityValue + 2);
-        driver.findElement(By.cssSelector(editField)).sendKeys(Keys.BACK_SPACE + newQuanityValue);
-        driver.findElement(By.cssSelector("div[class*='table-responsive'] > table > tbody > tr:nth-of-type(1) > td:nth-of-type(4) > div > span > button:nth-of-type(1)")).click();
-        double actualTotal = getFloatNumber(driver.findElement(By.cssSelector("#content > form > div > table > tbody > tr:nth-child(1) > td:nth-child(6)")).getText());
-        System.out.println("delete: " + driver.findElement(By.cssSelector("#content > h1")).getText());
-        Assert.assertEquals(actualTotal, expectedTotal);
-    }
-
-    @Test (priority = 4)
-    public void checkRefreshTotalWithCentsTest() throws  Exception {
-        driver.findElement(By.cssSelector("i[class='fa fa-shopping-cart']")).click();
-        quanityValue = Integer.parseInt(driver.findElement(By.cssSelector("#content > form > div > table > tbody > tr:nth-child(3) > td:nth-child(4) > div > input")).getAttribute("value"));
-        total = getFloatNumber(driver.findElement(By.cssSelector("div[class*='table-responsive'] > table > tbody > tr:nth-of-type(3) > td:nth-of-type(6)")).getText());
-        System.out.println("total" + total);
-        unitPrice = getFloatNumber(driver.findElement(By.cssSelector("div[class*='table-responsive'] > table > tbody > tr:nth-of-type(3) > td:nth-of-type(5)")).getText());
-        System.out.println("unitPrice" + unitPrice);
-        double expectedTotal = (double) Math.round((total + unitPrice) * 100) / 100;
-        System.out.println("expectedTotal" + expectedTotal);
-        driver.findElement(By.cssSelector("#content > form > div > table > tbody > tr:nth-child(3) > td:nth-child(4) > div > input")).clear();
-        String newQuanityValue = String.valueOf(quanityValue + 1);
-        driver.findElement(By.cssSelector("#content > form > div > table > tbody > tr:nth-child(3) > td:nth-child(4) > div > input")).sendKeys(Keys.BACK_SPACE + newQuanityValue);
-        driver.findElement(By.cssSelector("div[class*='table-responsive'] > table > tbody > tr:nth-of-type(3) > td:nth-of-type(4) > div > span > button:nth-of-type(1) > i")).click();
-        double actualTotal = getFloatNumber(driver.findElement(By.cssSelector("div[class*='table-responsive'] > table > tbody > tr:nth-of-type(3) > td:nth-of-type(6)")).getText());
-        System.out.println("delete: " + driver.findElement(By.cssSelector("#content > h1")).getText());
-        Assert.assertEquals(actualTotal, expectedTotal);
-        Assert.assertTrue(driver.findElement(By.cssSelector("div[id='content'] > p")).isEnabled());
-    }*/
-
 
 }
-
-
-//driver.findElement(By.cssSelector("a[href*='http://192.168.234.129/opencart/upload/index.php?route=common/home']")).click();
-
-
-        /*
-         //**************************** L O G I N 1 ********************************************************************
-        driver.get(OPEN_CART_URL + "index.php?route=account/login");
-        driver.findElement(By.id("input-email")).click();
-        driver.findElement(By.id("input-email")).sendKeys("yuriykril7773@gmail.com");
-        driver.findElement(By.id("input-password")).click();
-        driver.findElement(By.id("input-password")).sendKeys("qwerty" + Keys.ENTER);
-
-        //**************************** L O G I N 2 ********************************************************************
-
-        //**************************** A D D **************************************************************************
-        driver.get("http://192.168.234.129/opencart/upload/index.php?");
-        driver.findElement(By.cssSelector("#content > div.row > div:nth-child(1) > div > div.button-group > button:nth-child(1)")).click();
-        driver.findElement(By.cssSelector("#content > div.row > div:nth-child(4) > div > div.button-group > button:nth-child(1)")).click();
-        driver.findElement(By.cssSelector("#input-option226 > option:nth-child(2)")).click();
-        driver.findElement(By.cssSelector("#input-quantity")).click();
-        driver.findElement(By.cssSelector("#input-quantity")).sendKeys(Keys.BACK_SPACE + "2" + Keys.ENTER);
-        driver.findElement(By.id("button-cart")).click();
-        driver.findElement(By.cssSelector("a[href*='http://192.168.234.129/opencart/upload/index.php?route=common/home']")).click();
-        driver.findElement(By.xpath("//a[contains(text(),'Phones & PDAs')]")).click();
-        driver.findElement(By.linkText("Palm Treo Pro")).click();
-        driver.findElement(By.id("input-quantity")).click();
-        driver.findElement(By.id("input-quantity")).sendKeys(Keys.BACK_SPACE + "4");
-        driver.findElement(By.id("input-quantity")).click();
-        driver.findElement(By.id("button-cart")).click();*/
--- src/test/java/com/softserve/edu/ShoppingCartPageTest.java
+++ src/test/java/com/softserve/edu/ShoppingCartPageTest.java
@@ -1,15 +1,13 @@
 package com.softserve.edu;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class ShoppingCartPageTest extends LoginAndAddProductsTest {
-    private int quanityValue;
+    private int quantityValue;
     private double total;
     private double unitPrice;
     protected String refreshButton = "div[class*='table-responsive'] > table > tbody > tr:nth-of-type(1) > td:nth-of-type(4) > div > span > button:nth-of-type(1)";
@@ -22,16 +20,6 @@ public class ShoppingCartPageTest extends LoginAndAddProductsTest {
         double d = Double.parseDouble(s);
         return (double) Math.round(d * 100) / 100;
     }
-
-   /* @BeforeMethod (dependsOnMethods = "positiveQuanityDataAndRefresh")
-    public void beforeRefresh(){
-        driver.findElement(By.cssSelector("i[class='fa fa-shopping-cart']")).click();
-        WebElement productQuanity = driver.findElement(By.cssSelector(editField));
-        int productCount = Integer.parseInt(productQuanity.getText());
-        WebElement productTotal = driver.findElement(By.cssSelector(totalField));
-        total = getFloatNumber(productTotal.getText());
-        unitPrice = getFloatNumber(driver.findElement(By.cssSelector(unitPriceField)).getText());
-    }*/
 
     @DataProvider
     public Object[][] positiveQuanityDataAndRefresh() {
@@ -49,38 +37,37 @@ public class ShoppingCartPageTest extends LoginAndAddProductsTest {
     public void refreshTest(int plusQuanity, double expectedResult) {
         driver.findElement(By.cssSelector("i[class='fa fa-shopping-cart']")).click();
         WebElement quanityField = driver.findElement(By.cssSelector(editField));
-        String newQuanity = String.valueOf(Integer.parseInt(quanityField.getAttribute("value")) + plusQuanity);
+        String newQuantity = String.valueOf(Integer.parseInt(quanityField.getAttribute("value")) + plusQuanity);
         quanityField.clear();
-        quanityField.sendKeys(newQuanity);
+        quanityField.sendKeys(newQuantity);
         driver.findElement(By.cssSelector(refreshButton)).click();
         double actual = getFloatNumber(driver.findElement(By.cssSelector(totalField)).getText());
         Assert.assertEquals(actual, expectedResult);
     }
 
-    @Test
-    public void deleteItemTest(int countOfRows, int expectedResult){
-        int actual = driver.findElements(By.cssSelector("")).size();
-        Assert.assertEquals(actual, expectedResult);
+    @DataProvider
+    public Object[][] rowDelete() {
+        //addProductsTest();
+        driver.findElement(By.cssSelector("i[class='fa fa-shopping-cart']")).click();
+        int rowInTable = driver.findElements(By.xpath(".//form/div/table[@class='table table-bordered']/tbody/tr")).size();
+        String deletePenultimateItemSelector = "//*[@id='content']/form/div/table/tbody/tr["+ (rowInTable-1)+"]/td[4]/div/span/button[2]";
+        String productName = driver.findElement(By.xpath("//*[@id='content']/form/div/table/tbody/tr[" + (rowInTable-1) + "]/td[2]/a")).getText();
+        return new Object[][]{
+                {deletePenultimateItemSelector, rowInTable-1, productName },
+        };
+    }
+    @Test (dataProvider = "rowDelete", priority = 4)
+    public void deleteItemTest(String deleteSelector, int expectedRowCount, String expectedNotFindProductName){
+        driver.findElement(By.cssSelector("i[class='fa fa-shopping-cart']")).click();
+        driver.findElement(By.xpath(deleteSelector)).click();
+        driver.navigate().refresh();
+        String actual = driver.findElement(By.xpath("//*[@id='content']/form/div/table/tbody/tr[" + (expectedRowCount) + "]/td[2]/a")).getText();
+        System.out.println(actual);
+        Assert.assertNotEquals(actual, expectedNotFindProductName);
+        int actualRowCount = driver.findElements(By.xpath(".//form/div/table[@class='table table-bordered']/tbody/tr")).size();
+        Assert.assertEquals(actualRowCount, expectedRowCount);
     }
 
-       /* driver.findElement(By.cssSelector("i[class='fa fa-shopping-cart']")).click();
-        quanityValue = Integer.parseInt(driver.findElement(By.cssSelector(editField)).getAttribute("value"));
-        total = getFloatNumber(driver.findElement(By.cssSelector(totalField)).getText());
-        unitPrice = getFloatNumber(driver.findElement(By.cssSelector(unitPriceField)).getText());
 
-        double expectedTotal = (double) Math.round((total + unitPrice + unitPrice) * 100) / 100;
-        driver.findElement(By.cssSelector(editField)).clear();
-        String newQuanityValue = String.valueOf(quanityValue + inputValue);
-        driver.findElement(By.cssSelector(editField)).sendKeys(Keys.BACK_SPACE + newQuanityValue);
-        driver.findElement(By.cssSelector("div[class*='table-responsive'] > table > tbody > tr:nth-of-type(1) > td:nth-of-type(4) > div > span > button:nth-of-type(1)")).click();
-        double actualTotal = getFloatNumber(driver.findElement(By.cssSelector(totalField)).getText());
-        Assert.assertEquals(actualTotal, expectedTotal);*/
-        /*
-        driver.findElement(By.cssSelector(selector)).click();
-        driver.findElement(By.cssSelector(selector)).sendKeys(inputValue);
-        driver.findElement(By.id("description")).click();
-        driver.findElement(By.id("input-search")).sendKeys(Keys.ENTER);
-        WebElement webElement = driver.findElement(By.xpath(selector));
-        String actual = webElement.getText();
-        Assert.assertEquals(actual, expectedResult);*/
+        //$x(".//tbody/tr/td[2]/a")
 }
