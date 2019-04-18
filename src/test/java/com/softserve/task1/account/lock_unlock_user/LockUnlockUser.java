6428afbe4a94f5e20f1af22444e101155021cb6d
AntokhivYura <antokhivyura@gmail.com>
2019-04-18 13:49:45 +0300

    Add test to verify logic for lock\unlock user via DB

--- src/test/java/com/softserve/task1/account/lock_unlock_user/LockUnlockUser.java
+++ src/test/java/com/softserve/task1/account/lock_unlock_user/LockUnlockUser.java
@@ -1,103 +1,69 @@
 package com.softserve.task1.account.lock_unlock_user;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.concurrent.TimeUnit;
 
 public class LockUnlockUser {
     private WebDriver driver;
-    private ArrayList<String> tabs;
+    private Connection connection;
 
-    private final String URL = "http://192.168.227.129/opencart/upload/admin/";
-    private final String adminLogin = "admin";
-    private final String adminPassword = "admin";
+    private String db_url = "jdbc:mysql://192.168.227.130:3306/opencart?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+
+    private final String setStatusDisabled = "UPDATE opencart.oc_customer SET status = '0' WHERE email like 'john.wick.test%'";
+    private final String setStatusEnabled = "UPDATE opencart.oc_customer SET status = '1' WHERE email like 'john.wick.test%'";
+
     private final String userMail = "john.wick.test@ukr.net";
     private final String userPassword = "qwerty";
+
     private final String failureMessage = "Warning: No match for E-Mail Address and/or Password.";
     private final String successMessage = "My Account";
 
     @BeforeClass
     public void openBrowser() {
         //Set Properties
-//        System.setProperty("webdriver.chrome.driver", "./lib/drivers/chromedriver.exe");
-//        System.getProperty("webdriver.chrome.driver");
-        System.setProperty("webdriver.chrome.driver",
-                this.getClass().getResource("/chromedriver-windows-32bit.exe").getPath());
+        System.setProperty("webdriver.chrome.driver", "./lib/drivers/chromedriver.exe");
+        System.getProperty("webdriver.chrome.driver");
         //Create new WebDriver object
         driver = new ChromeDriver();
         //Set window --> maximize
         driver.manage().window().maximize();
         //Set timeout 20 sec
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //Open login page
+        driver.get("http://192.168.227.130/opencart/upload/index.php?route=account/login");
     }
 
     @Test(priority = 1)
-    public void openAdminLoginPage() {
-        //Open url --> opencart
-        driver.get(URL);
-    }
-
-    @Test(priority = 2)
-    public void loginIntoAdminPanel() {
-        //Input login
-        driver.findElement(By.id("input-username")).click();
-        driver.findElement(By.id("input-username")).clear();
-        driver.findElement(By.id("input-username")).sendKeys(adminLogin);
-        //
-        //Input password
-        driver.findElement(By.id("input-password")).click();
-        driver.findElement(By.id("input-password")).clear();
-        driver.findElement(By.id("input-password")).sendKeys(adminPassword);
-        //
-        //Click 'Login' button
-        driver.findElement(By.cssSelector("button[class*='btn']")).click();
-    }
-
-    @Test(priority = 3)
-    public void openCustomersList() {
-        //Create action
-        Actions builder = new Actions(driver);
-        //
-        //Open Customer menu
-        builder.moveToElement(driver.findElement(By.cssSelector("li[id='menu-customer'] > a"))).perform();
-        //
-        //Click on 'Customers' button
-        builder.moveToElement(driver.findElement(By.xpath("(//a[contains(text(),'Customers')])[2]"))).click().perform();
+    public void getConnection() {
+        try {
+            connection = DriverManager.getConnection(db_url, "lv395", "Lv395_Taqc");
+            System.out.println("Successfully connected to db");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test(priority = 4)
     public void editCustomerStatus() {
-        //Click on Edit button
-        driver.findElement(By.cssSelector("i[class*='fa-pencil']")).click();
-        //
-        //Change user status to 'Disabled'
-        new Select(driver.findElement(By.id("input-status"))).selectByVisibleText("Disabled");
-        //
-        //Save changes
-        driver.findElement(By.cssSelector("i[class*='fa-save']")).click();
-    }
-
-    @Test(priority = 5)
-    public void openLoginPage() {
-        //Open new empty tab using javascript
-        ((JavascriptExecutor) driver).executeScript("window.open(\"http://192.168.227.129/opencart/upload/index.php?route=account/login\")");
-        //
-        //Create array of tabs
-        tabs = new ArrayList<>(driver.getWindowHandles());
-        //
-        //Switches to new tab
-        driver.switchTo().window(tabs.get(1));
+        try {
+            PreparedStatement ps = connection.prepareStatement(setStatusDisabled);
+            ps.executeUpdate();
+            System.out.println("Status changed to Disabled");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test(priority = 6)
@@ -125,28 +91,15 @@ public class LockUnlockUser {
         Assert.assertEquals(actual, failureMessage);
     }
 
-    @Test(priority = 7)
-    public void switchToAdminPanel() {
-        //Switch to first tab
-        driver.switchTo().window(tabs.get(0));
-    }
-
     @Test(priority = 8)
     public void rollbackCustomerStatus() {
-        //Click on Edit button
-        driver.findElement(By.cssSelector("i[class*='fa-pencil']")).click();
-        //
-        //Change user status to 'Enabled'
-        new Select(driver.findElement(By.id("input-status"))).selectByVisibleText("Enabled");
-        //
-        //Save changes
-        driver.findElement(By.cssSelector("i[class*='fa-save']")).click();
-    }
-
-    @Test(priority = 9)
-    public void switchToLoginPage() {
-        //Switches to login page
-        driver.switchTo().window(tabs.get(1));
+        try {
+            PreparedStatement ps = connection.prepareStatement(setStatusEnabled);
+            ps.executeUpdate();
+            System.out.println("Status changed to Enabled");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test(priority = 10)
@@ -174,15 +127,14 @@ public class LockUnlockUser {
         Assert.assertEquals(actual, successMessage);
     }
 
-    @Test(priority = 11)
-    public void closeTab() {
-        //Close second tab using javascript
-        ((JavascriptExecutor) driver).executeScript("window.close()");
-    }
-
     @AfterClass
     public void exit() {
         //Close driver
         driver.quit();
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
