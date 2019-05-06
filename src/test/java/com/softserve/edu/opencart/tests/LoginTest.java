e717ecd6a0a19b2b6b406261e59b71837111d47f
AntokhivYura <antokhivyura@gmail.com>
2019-05-06 12:11:21 +0300

    Refactored

--- src/test/java/com/softserve/edu/opencart/tests/LoginTest.java
+++ src/test/java/com/softserve/edu/opencart/tests/LoginTest.java
@@ -15,7 +15,7 @@ public class LoginTest extends ATestRunner {
     @DataProvider // (parallel = true)
     public Object[][] validUsers() {
         return new Object[][] {
-            { UserRepository.get().johnWick()},
+            { UserRepository.get().johnWick() },
         };
     }
 
