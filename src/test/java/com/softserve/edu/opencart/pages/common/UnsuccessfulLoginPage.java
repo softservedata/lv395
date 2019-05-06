12a9224ec20ca2d2e4d9af6583079a8ebe3edd53
AntokhivYura <antokhivyura@gmail.com>
2019-05-06 08:44:05 +0300

    Add mthods

--- src/test/java/com/softserve/edu/opencart/pages/common/UnsuccessfulLoginPage.java
+++ src/test/java/com/softserve/edu/opencart/pages/common/UnsuccessfulLoginPage.java
@@ -1,15 +1,31 @@
 package com.softserve.edu.opencart.pages.common;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class UnsuccessfulLoginPage extends LoginPage {
 
+    private WebElement unsuccessfulMessage;
+    public static final String ERROR_MESSAGE = "Warning: No match for E-Mail Address and/or Password.";
+
     public UnsuccessfulLoginPage(WebDriver driver) {
         super(driver);
+        initElements();
     }
 
+    private void initElements(){
+        unsuccessfulMessage = driver.findElement(By.cssSelector("div[class*='alert']"));
+    }
     // Page Object
 
+    public WebElement getUnsuccessfulMessage(){
+        return unsuccessfulMessage;
+    }
+
+    public String getUnsuccessfulMessageText(){
+        return getUnsuccessfulMessage().getText();
+    }
     // Functional
 
     // Business Logic
