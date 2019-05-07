dd62c44085e323fe56378806091d401d548f07c4
AntokhivYura <antokhivyura@gmail.com>
2019-05-07 15:32:57 +0300

    Add page messages

--- src/test/java/com/softserve/edu/opencart/pages/account/UnsuccessfulLoginPage.java
+++ src/test/java/com/softserve/edu/opencart/pages/account/UnsuccessfulLoginPage.java
@@ -1,6 +1,5 @@
 package com.softserve.edu.opencart.pages.account;
 
-import com.softserve.edu.opencart.pages.account.LoginPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -8,7 +7,8 @@ import org.openqa.selenium.WebElement;
 public class UnsuccessfulLoginPage extends LoginPage {
 
     private WebElement unsuccessfulMessage;
-    public static final String ERROR_MESSAGE = "Warning: No match for E-Mail Address and/or Password.";
+    public static final String LOGIN_PAGE_INCORRECT_CREDENTIALS_MESSAGE = "Warning: No match for E-Mail Address and/or Password.";
+    public static final String TOO_MUCH_LOGIN_ATTEMPTS = "Warning: Your account has exceeded allowed number of login attempts. Please try again in 1 hour.";
 
     public UnsuccessfulLoginPage(WebDriver driver) {
         super(driver);
