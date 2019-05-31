f5ea547989d1a605e104ff3d29fcf22b7947670e
Ira <iraratushniak@gmail.com>
2019-06-01 00:48:23 +0300

    allure added

--- src/test/java/com/softserve/edu/rest/test/CreateAndDeleteUserTest.java
+++ src/test/java/com/softserve/edu/rest/test/CreateAndDeleteUserTest.java
@@ -5,6 +5,10 @@ import com.softserve.edu.rest.data.UserRepository;
 import com.softserve.edu.rest.service.AdminService;
 import com.softserve.edu.rest.service.GuestService;
 import com.softserve.edu.rest.service.UserService;
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
@@ -26,6 +30,10 @@ public class CreateAndDeleteUserTest {
      * Expected result: user will be created
      */
     @Test()
+    @Severity(SeverityLevel.CRITICAL)
+    @Description(" Test for creating new user without admin rights" +
+            "Expected result: user will be created")
+    @Story("Create user")
     public void createUserTest() {
         User newUser = UserRepository.newUserWithoutAdminRihts();
         adminService.createUser(newUser);
@@ -46,6 +54,10 @@ public class CreateAndDeleteUserTest {
      * Expected result: user will be created
      */
     @Test()
+    @Severity(SeverityLevel.CRITICAL)
+    @Description(" Test for creating new user with admin rights" +
+            "Expected result: user will be created")
+    @Story("Create user")
     public void createAdminTest() {
         User newAdmin = UserRepository.newUserWithAdminRihts();
         adminService.createUser(newAdmin);
@@ -64,6 +76,10 @@ public class CreateAndDeleteUserTest {
      * Expected result: user won`t be created
      */
     @Test
+    @Severity(SeverityLevel.CRITICAL)
+    @Description(" Test for creating existing user" +
+            "Expected result: user won`t be created")
+    @Story("Create user")
     public void createExistingUserTest() {
         User user = UserRepository.getUser1();
         //Check user won`t be created
@@ -84,6 +100,10 @@ public class CreateAndDeleteUserTest {
      * Expected result: user will be deleted.
      */
     @Test(dataProvider = "usersWeWantToRemove")
+    @Severity(SeverityLevel.CRITICAL)
+    @Description(" Delete user without admin rights" +
+            "Expected result: user will be deleted.")
+    @Story("Delete user")
     public void deleteUserTest(User userWeWantToRemove) {
         adminService.removeUser(userWeWantToRemove.getName());
         Assert.assertFalse(adminService.getAllUsers().contains(userWeWantToRemove.getName()));
@@ -94,9 +114,13 @@ public class CreateAndDeleteUserTest {
      * Expected result: user will be deleted.
      */
     @Test()
-    public void deleteNotExistingTest() {
+    @Severity(SeverityLevel.CRITICAL)
+    @Description(" Delete user with admin rights" +
+            "Expected result: user will be deleted.")
+    @Story("Delete user")
+    public void deleteNotExistingUserTest() {
         User notExistingUser = UserRepository.notExistingUser();
-        Boolean userIsDeleted=adminService.removeUser(notExistingUser.getName());
+        Boolean userIsDeleted = adminService.removeUser(notExistingUser.getName());
         Assert.assertTrue(userIsDeleted);
     }
 
